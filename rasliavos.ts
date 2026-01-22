81bdaedbce815eb0b0f402a6443d17fe699b4445
paulie-of-punskas <pbernecki@protonmail.com>
2026-01-22 10:11:08 +0100

    Pridejau testus

--- rasliavos.ts
+++ rasliavos.ts
@@ -1,8 +1,10 @@
 class LanguageLTS {
     #language: string;
+    #ltsVersions: Array<number>;
 
-    constructor(language: string) {
+    constructor(language: string, ltsVersions: Array<number>) {
         this.#language = language;
+        this.#ltsVersions = ltsVersions;
     };
 
     get language(): string {
@@ -12,25 +14,46 @@ class LanguageLTS {
     set language(value: string) {
         this.#language = value;
     }
-};
 
-async function validateResult(jsonObject: JSON) {
+    get ltsVersions(): Array<number> {
+        return this.#ltsVersions;
+    };
+
+    set ltsVersions(value: Array<number>) {
+        this.#ltsVersions = value;
+    };
+
+    validateJSONinput(jsonObject: JSON): void {
     /**
-     *
+     * @param {JSON} jsonObject - JSON returned from sendRequest().
      *
     */
-}
 
-async function sendRequest(language: string) {
+    }
+};
+
+
+async function sendRequest(language: string): Promise<{}> {
     /**
-     * @param {string} laguage - name of the language.
+     * @param {string} language - name of the language.
      * @returns {JSON} JSON object.
      */
-    const url = "https://endoflife.date/api/v1/products/" + language.toLowerCase();
+
+    const url = "https://endoflife.date/api/v1/products/`${language.toLowerCase()`";
+    const header = new Headers();
+    header.append("Content-Type", "application/json");
+
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            method: "GET",
+            headers: header,
+        });
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+            console.error(`Response status: ${response.status}`);
+            return {};
+        } else if (response.status == 404) {
+            console.error(`${language} was not found on https://endoflife.date.`);
+            return {};
         };
         const result = await response.json();
         return result;
@@ -38,17 +61,22 @@ async function sendRequest(language: string) {
         if (error instanceof Error) {
             console.error(`Caught an error: ${error.message}`);
         }
+        return {};
     };
 };
 
-function parseRequest() {};
-
 function getNlatestVersions(numOfVersions: number): Array<number> {
+    /**
+     * @param {number} numOfVersions - how many LTS versions to retrieve. If it exceeds supported versions,
+     * then return max supported number of versions.
+     */
     let ltsVersions: Array<number> = [];
     return ltsVersions;
 };
 
 function run(language: string) {
-    const languageLTS = new LanguageLTS(language);
+    const languageLTS = new LanguageLTS(language, new Array<number>);
+    let returnedJSON = sendRequest("python");
+    // languageLTS.validateJSONinput(returnedJSON);
 };
 
