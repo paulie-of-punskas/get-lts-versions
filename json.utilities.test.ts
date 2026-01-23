0acb307fd2d9de00cf2f4359236c3958252d1e19
paulie-of-punskas <pbernecki@protonmail.com>
2026-01-23 19:24:15 +0100

    Atnaujinau testus su describe() ir it()

--- json.utilities.test.ts
+++ json.utilities.test.ts
@@ -1,25 +1,28 @@
 import { EOLresponse } from "./classes";
 import { isJSONok, getNlatestVersions } from "./json.utilities";
-import {exec} from 'child_process';
-
-// import fs from "fs";
 
 import testDataGo from "./tests/example_return_go.json";
 import testDataPython from "./tests/example_return_python.json";
 
-test("isJSONok, expect true - read example_return_go", () => {
+describe("isJSONok():", () => {
     const testJSONdata = testDataGo;
-    expect(isJSONok(JSON.stringify(testJSONdata))).toBe(true);
-});
 
-test("isJSONok, expect false - read empty JSON", () => {
-    const testJSONdata = JSON.stringify("");
-    expect(isJSONok(testJSONdata)).toBe(false);
-});
+    it("should return true, when input is example_return_go", () => {
+        expect(isJSONok(JSON.stringify(testJSONdata))).toBe(true);
+    });
 
-test("isJSONok, expect false - {}", () => {
-    const jsonInput = JSON.stringify("{}");
-    expect(isJSONok(JSON.stringify(jsonInput))).toBe(false);
+    it("should return false, when input is empty JSON", () => {
+        expect(isJSONok(JSON.stringify(""))).toBe(false);
+    });
+
+    it("should return false, when input is '{}'", () => {
+        expect(isJSONok(JSON.stringify("{}"))).toBe(false);
+    });
+
+    it("should return false, when input is {'result': ''}", () => {
+        expect(isJSONok(JSON.stringify({result: ''}))).toBe(false);
+    });
+
 });
 
 test("isJSONok, expect false - {result = ''}", () => {
@@ -33,20 +36,22 @@ test("isJSONok, expect false - {result = {releases = ''}, }", () => {
     expect(isJSONok(JSON.stringify(jsonFile))).toBe(false);
 });
 
-test("getNlatestVersions, Python, N=3 - expect returned array, size of 3", () => {
-    const result: Array<string> = getNlatestVersions(JSON.stringify(testDataPython), 3);
-    expect(result).toHaveLength(3);
+describe("getNlatestVersions():", () => {
+    it("should return array sized of 3, for Python and N = 3", () => {
+        const result: Array<string> = getNlatestVersions(JSON.stringify(testDataPython), 3);
+        expect(result).toHaveLength(3);
+    });
+
+    it("should return array sized of 2, for Golang and N = 3", () => {
+        const result: Array<string> = getNlatestVersions(JSON.stringify(testDataGo), 3);
+        expect(result).toHaveLength(2);
+    });
 });
 
-test("getNlatestVersions, Golang, N=3 - expect returned array, size of 2", () => {
-    const result: Array<string> = getNlatestVersions(JSON.stringify(testDataGo), 3);
-    expect(result).toHaveLength(2);
-});
 
 //////////////////////////////////////////////////////////
 // Integration tests with mocks
 /////////////////////////////////////////////////////////
-import * as https from "https";
 
 // Below test will only work, if local server is running
 // Can be insantiated with `python3 -m http.server` within tests/
@@ -78,10 +83,10 @@ import * as https from "https";
 //     };
 // });
 
-import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
+import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 import axios from "axios";
 
-describe('Python HTTP Server Tests', () => {
+describe("Python HTTP Server Tests", () => {
     let pythonServer: ChildProcessWithoutNullStreams | null = null;
     const serverPort = 8000;
 
@@ -107,165 +112,10 @@ describe('Python HTTP Server Tests', () => {
     it("should serve files correctly", async () => {
         try {
             const response = await axios.get("http://localhost:8000/example_return_go.json");
-            console.log(response.data);
             expect(response.status).toBe(200);
+            expect(isJSONok(JSON.stringify(response.data))).toBe(true);
         } catch (error) {
             console.error(`File serving failed: ${error}`);
         }
     });
 });
-
-
-// jest.mock("https");
-
-// describe("Extract data", () => {
-//     beforeEach(() => {
-//         jest.clearAllMocks();
-//     });
-
-//     describe("HTTP source", () => {
-//         it("should extract data from HTTPS URL", async () => {
-//             const mockJSONdata = JSON.stringify({
-//                 "schema_version": "1.2.0",
-//                 "generated_at": "2026-01-22T00:11:38+00:00",
-//                 "last_modified": "2026-01-16T00:10:25+00:00",
-//                 "result": {
-//                     "name": "go",
-//                     "aliases": ["golang"],
-//                     "label": "Go",
-//                     "category": "lang",
-//                     "tags": ["google", "lang"],
-//                     "versionCommand": "go version",
-//                     "identifiers": [{"type": "repology", "id": "go"}],
-//                     "labels": {
-//                         "eoas": null,
-//                         "discontinued": null,
-//                         "eol": "Supported",
-//                         "eoes": null
-//                     },
-//                     "links": {
-//                         "icon": "https://cdn.jsdelivr.net/npm/simple-icons/icons/go.svg",
-//                         "html": "https://endoflife.date/go",
-//                         "releasePolicy": "https://go.dev/doc/devel/release#policy"
-//                     },
-//                     "releases": [
-//                     {
-//                         "name": "1.25",
-//                         "codename": null,
-//                         "label": "1.25",
-//                         "releaseDate": "2025-08-12",
-//                         "isLts": false,
-//                         "ltsFrom": null,
-//                         "isEol": false,
-//                         "eolFrom": null,
-//                         "isMaintained": true,
-//                         "latest": {
-//                             "name": "1.25.6",
-//                             "date": "2026-01-15",
-//                             "link": "https://go.dev/doc/devel/release#go1.25.minor"
-//                         },
-//                         "custom": null
-//                     },
-//                     {
-//                         "name": "1.24",
-//                         "codename": null,
-//                         "label": "1.24",
-//                         "releaseDate": "2025-02-11",
-//                         "isLts": false,
-//                         "ltsFrom": null,
-//                         "isEol": false,
-//                         "eolFrom": null,
-//                         "isMaintained": true,
-//                         "latest": {
-//                             "name": "1.24.12",
-//                             "date": "2026-01-15",
-//                             "link": "https://go.dev/doc/devel/release#go1.24.minor"
-//                         },
-//                         "custom": null
-//                     }]}
-//                 });
-//                 const mockResponse = {
-//                     on: jest.fn((event, handler) => {
-//                         if (event === "data") {
-//                             handler(mockJSONdata)
-//                         } else if (event === "end") {
-//                             handler();
-//                         }
-//                         return mockResponse;
-//                     }),
-//                 };
-
-//                 const mockRequest = {
-//                     on: jest.fn().mockReturnThis(),
-//                 };
-
-//                 (https.get as jest.Mock).mockImplementation((url, callback) => {
-//                     callback(mockResponse);
-//                     return mockRequest;
-//                 })
-
-//                 const result = await extractData("https://example.com/data.json");
-
-//                 expect(result).toEqual(
-//                     {
-//                 "schema_version": "1.2.0",
-//                 "generated_at": "2026-01-22T00:11:38+00:00",
-//                 "last_modified": "2026-01-16T00:10:25+00:00",
-//                 "result": {
-//                     "name": "go",
-//                     "aliases": ["golang"],
-//                     "label": "Go",
-//                     "category": "lang",
-//                     "tags": ["google", "lang"],
-//                     "versionCommand": "go version",
-//                     "identifiers": [{"type": "repology", "id": "go"}],
-//                     "labels": {
-//                         "eoas": null,
-//                         "discontinued": null,
-//                         "eol": "Supported",
-//                         "eoes": null
-//                     },
-//                     "links": {
-//                         "icon": "https://cdn.jsdelivr.net/npm/simple-icons/icons/go.svg",
-//                         "html": "https://endoflife.date/go",
-//                         "releasePolicy": "https://go.dev/doc/devel/release#policy"
-//                     },
-//                     "releases": [
-//                     {
-//                         "name": "1.25",
-//                         "codename": null,
-//                         "label": "1.25",
-//                         "releaseDate": "2025-08-12",
-//                         "isLts": false,
-//                         "ltsFrom": null,
-//                         "isEol": false,
-//                         "eolFrom": null,
-//                         "isMaintained": true,
-//                         "latest": {
-//                             "name": "1.25.6",
-//                             "date": "2026-01-15",
-//                             "link": "https://go.dev/doc/devel/release#go1.25.minor"
-//                         },
-//                         "custom": null
-//                     },
-//                     {
-//                         "name": "1.24",
-//                         "codename": null,
-//                         "label": "1.24",
-//                         "releaseDate": "2025-02-11",
-//                         "isLts": false,
-//                         "ltsFrom": null,
-//                         "isEol": false,
-//                         "eolFrom": null,
-//                         "isMaintained": true,
-//                         "latest": {
-//                             "name": "1.24.12",
-//                             "date": "2026-01-15",
-//                             "link": "https://go.dev/doc/devel/release#go1.24.minor"
-//                         },
-//                         "custom": null
-//                     }]}
-//                 });
-//         });
-//     })
-// })
