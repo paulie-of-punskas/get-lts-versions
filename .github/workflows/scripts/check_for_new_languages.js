46e59bd7d777af17569d0011ac6916cbe30e92fb
paulie-of-punskas <pbernecki@ik.me>
2026-09-10 20:59:59 +0200

    GHA - check for language updates

---
+++ .github/workflows/scripts/check_for_new_languages.js
@@ -0,0 +1,31 @@
+import EOL_PRODUCTS from "./end_of_life_products.json" with { type: "json" };
+import LANGUAGE_NAMES from "../../../src/assets/language_names.json" with { type: "json" };
+
+function returnLanguageNamesFromEOL() {
+    let languageNames = [];
+    const languages = EOL_PRODUCTS.result;
+    for (let j = 0; j < languages.length; j++) {
+        if (languages[j].category == "lang") {
+            languageNames.push(languages[j].name);
+        }
+    }
+    return languageNames;
+}
+
+function convertLanguageNamesToSet() {
+    return new Set(Object.keys(LANGUAGE_NAMES.aliases));
+}
+
+function areLanguageNamesUpToDate() {
+    const languageNames = returnLanguageNamesFromEOL();
+    const languageNamesSet = convertLanguageNamesToSet();
+    let areLanguagesUpToDate = true;
+
+    for (let j = 0; j < languageNames.length; j++) {
+        if (!languageNamesSet.has(languageNames[j])) {
+            console.log(`Following language is missing: ${languageNames[j]}`);
+            areLanguageNamesUpToDate = false;
+        }
+    }
+    return areLanguageNamesUpToDate;
+}
